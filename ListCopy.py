84c04b0f6a8ffc772503391775338e1aa38cda15
TomGauss <32924623+TomGauss@users.noreply.github.com>
2019-03-17 10:39:12 +0000

    Update ListCopy.py

--- ListCopy.py
+++ ListCopy.py
@@ -1,6 +1,12 @@
 #if you do not use copy to save a list
 #then using the extend would change the original list
 
+#implicit explnation:
+#I guess this might have something to with python language feature
+#when you use list3 = list1, you might use the same memory with a different name
+#this is probably why you change list3 then list1 changes as well. 
+#so I guess using copy a new memory was given. 
+
 #not copy a list would change the original list
 list1 = [1, 2, 3, 4]
 print("list1 is:\n", list1)
